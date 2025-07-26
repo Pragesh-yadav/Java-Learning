9b68fd579ba81b5382014b21d393fd984a7208dc
pragesh-yadav <drop2pragesh@gmail.com>
2025-07-26 18:13:42 +0530

    Declare constant value to a variable

---
+++ Getting_Started/Constant/Constant.java
@@ -0,0 +1,12 @@
+package Getting_Started.Constant;
+
+public class Constant {
+    public static void main(String[] args){
+        final double PI = 3.141592; //Declare constant value of a variable use final key word.
+        PI = 3; // This will give error "java: cannot assign a value to final variable PI"
+        System.out.println(PI);
+        
+
+    }
+    
+}
