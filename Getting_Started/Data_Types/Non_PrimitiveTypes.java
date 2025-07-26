a5871f5fb02c90fdf1e3486b1b20c549918e1db3
pragesh-yadav <drop2pragesh@gmail.com>
2025-07-26 17:42:08 +0530

    Learning Non Primitive Data Types in Java String

---
+++ Getting_Started/Data_Types/Non_PrimitiveTypes.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Non_PrimitiveTypes {
+    public static void main(String[] args){
+        // Strings, Arrays, Classes, Interfaces, etc. are non-primitive data types in Java.
+        String str = "Hi How are you";
+        System.out.println(str.charAt(0));
+        System.out.println(str);
+
+        String Str2 = """
+                'Harry Potter and the Philoshpher's /stone
+                Harry Potter and the Chamber of Secertes
+                Harry Potter and the Prisnoer of Azkaban'
+                """;
+
+        System.out.println(Str2);
+
+
+    }
+    
+}
