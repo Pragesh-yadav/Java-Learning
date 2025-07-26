ba6e599addb1df9d7d1c29695ca92d5de7e2d32c
pragesh-yadav <drop2pragesh@gmail.com>
2025-07-26 23:04:58 +0530

    Reading input using Scanner

---
+++ Getting_Started/Reading_Inputs/readingInputs.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class readingInputs {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // int x = sc.nextInt();
+        // byte y = sc.nextByte();
+        // long l = sc.nextLong();
+        // short s = sc.nextShort();
+
+        // String str = sc.next(); // It only read first word of the string
+        // // if we want to read whole string then use nextLine()
+
+        // String str2 = sc.nextLine();
+        // System.out.println(str2);
+
+        // boolean bool = sc.nextBoolean();
+        // System.err.println(bool);
+
+        char c = sc.next().charAt(0); 
+        System.out.println(c);
+
+    }
+    
+}
