9143bb343245148fc90b713f153c429a820b51ec
pragesh-yadav <drop2pragesh@gmail.com>
2025-07-27 20:27:01 +0530

    Relational Operators

---
+++ OpratorsAndControlStatement/Relational.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Relational {
+    public static void main(String[] args) {
+        /* 
+        Operator             Description                Example
+        ==                   Is equal to                10==5 return false
+        >                    Greater than               10 > 5 return true
+        <                    Less than                  10 < 5 return false
+        >=                   Greater than or equal to   10 >= 5 return true
+        <=                   Less than or equal to      10 <= 5 return false
+        !=                   Not equal to               10 != 5 return true
+        
+         */
+
+    int x = 10;
+    int y = 5;
+
+    // == Operator
+    System.out.println(x == y);// return false
+
+    // > Operator
+    System.out.println(x > y); // return true
+
+    // < Operator
+    System.out.println(x < y); // return false
+
+    // >= Operator
+    System.out.println(x >= y); // return true
+
+    // <= Operator
+    System.out.println(x <= y); // return false
+
+    // != Operator 
+    System.out.println(x != y); // return true
+
+
+
+        
+    }
+    
+}
