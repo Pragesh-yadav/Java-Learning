b78ceed0dd92e4dadccedcc41a3ba7a41d3294e1
pragesh-yadav <drop2pragesh@gmail.com>
2025-07-28 22:53:56 +0530

    Added comment why use Ternary Operator

--- OpratorsAndControlStatement/ternaryOperator.java
+++ OpratorsAndControlStatement/ternaryOperator.java
@@ -5,7 +5,7 @@ public class ternaryOperator{
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
 
-        String ans = num%2 == 0 ? "Even": "Odd";
+        String ans = num%2 == 0 ? "Even": "Odd"; // Ternanry Operator is use to write single line code avoid to use if else condition statement. we can use both if else or ternary.
         System.out.println(ans);
     }
 
