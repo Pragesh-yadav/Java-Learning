27091845348c6e934704ad1bccc945f5b2d5378e
pragesh-yadav <drop2pragesh@gmail.com>
2025-07-29 23:39:20 +0530

    Logical Not operator work like true or False

---
+++ OpratorsAndControlStatement/LogicalNot.java
@@ -0,0 +1,10 @@
+public class LogicalNot {
+    public static void main(String[] args) {
+        boolean flag = true; // When it is true Nothing will return or print.
+        if(!flag){
+            System.out.println("Logical NOT ");
+
+        }
+    }
+    
+}
