600fab38e1cc667d08a19484f99b911edf57c10f
pragesh-yadav <drop2pragesh@gmail.com>
2025-08-01 22:39:36 +0530

    While Loop

---
+++ Loops/WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=10){ // while loops is used to execute a specific block of code untill a certain condition is met.
+            System.out.print(i + " ");
+            i++; // update
+
+        }
+    }
+}
